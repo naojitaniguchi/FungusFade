c0cd68d59dacb7f185f46ed6ded570970d7c6ecc
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-12-19 21:58:35 +0900

    First commit.

---
+++ Assets/SetCharacterScale.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCharacterScale : MonoBehaviour
+{
+    public float scale = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setScale()
+    {
+        string holderName = gameObject.name + " holder";
+
+        Debug.Log(holderName);
+
+        GameObject holder = GameObject.Find(holderName);
+
+        Vector2 sd = holder.GetComponent<RectTransform>().sizeDelta;
+        sd.x *= scale;
+        sd.y *= scale;
+        holder.GetComponent<RectTransform>().sizeDelta = sd;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FungusFade
+[![youtube](https://img.youtube.com/vi/6kqd3vja4DY/0.jpg)](http://www.youtube.com/watch?v=6kqd3vja4DY "FungusFade")
